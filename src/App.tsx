270392da124558cdf9bbf027a08fcee71d74a6d4
dipanshuhappy <dipanshuhappy@gmail.com>
2022-11-29 15:56:28 +0530

    homsfd

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/details" element={<Details />} />
       </Routes>
