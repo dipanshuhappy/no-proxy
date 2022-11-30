e2e4fc595dafa1610b0e7670ba956af0f8e96141
dipanshuhappy <dipanshuhappy@gmail.com>
2022-11-30 20:45:34 +0530

    added contract

--- src/contract.ts
+++ src/contract.ts
@@ -2,235 +2,229 @@ import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 import { Contract } from "web3-eth-contract";
 
-const ABI = 
-[
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_roll",
-				"type": "string"
-			}
-		],
-		"name": "mark_roll",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_enroll",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_batch",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_attendance",
-				"type": "uint256"
-			}
-		],
-		"name": "Register",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [],
-		"name": "All_Students",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "enroll",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "name",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "batch",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "attendence",
-						"type": "uint256"
-					}
-				],
-				"internalType": "struct attendence.Student[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "Array_Size",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_roll",
-				"type": "string"
-			}
-		],
-		"name": "check_roll",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "stud_details",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "enroll",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "batch",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "attendence",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_x",
-				"type": "string"
-			}
-		],
-		"name": "Student_Details",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "enroll",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "name",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "batch",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "attendence",
-						"type": "uint256"
-					}
-				],
-				"internalType": "struct attendence.Student",
-				"name": "",
-				"type": "tuple"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"name": "students1",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "enroll",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "batch",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "attendence",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]
-let contract: Contract | null = null;let run = false;
+const ABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_s_id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_faculty_id",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_password",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_start_time",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_end_time",
+        type: "uint256",
+      },
+    ],
+    name: "create_session",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_s_id",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_password",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_st_id",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "current_time",
+        type: "uint256",
+      },
+    ],
+    name: "mark_attendance",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+    ],
+    name: "register_faculty",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+    ],
+    name: "register_student",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "faculty_details",
+    outputs: [
+      {
+        internalType: "string",
+        name: "id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_s_id",
+        type: "string",
+      },
+    ],
+    name: "get_session_students",
+    outputs: [
+      {
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "session_details",
+    outputs: [
+      {
+        internalType: "string",
+        name: "s_id",
+        type: "string",
+      },
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "id",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+        ],
+        internalType: "struct Attendance.Faculty",
+        name: "faculty",
+        type: "tuple",
+      },
+      {
+        internalType: "uint256",
+        name: "password",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "start_time",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "end_time",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "student_details",
+    outputs: [
+      {
+        internalType: "string",
+        name: "id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+let contract: Contract | null = null;
+let run = false;
+let web3 = new Web3();
 export const getContract = async () => {
   if (!run) {
-    const web3 = new Web3(
+    web3 = new Web3(
       // @ts-ignore
       window.ethereum
     );
@@ -239,9 +233,11 @@ export const getContract = async () => {
     await window.ethereum.enable();
     contract = new web3.eth.Contract(
       ABI as AbiItem[],
-      "0xe3c875d373902d421a57ad813ad2fe3d2aafab13"
+      "0xC62A30A89C0f8a9c6e72087988356bac55dc177F"
     );
-    run=true;
+    run = true;
   }
   return contract;
 };
+export const getWeb3 = () => web3;
+export const getAddress = async () => (await getWeb3().eth.getAccounts())[0];
