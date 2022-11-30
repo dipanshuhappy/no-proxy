7a1592d3f9cfb4a8cf8dc62c0bd39c0a8b3f298a
dipanshuhappy <dipanshuhappy@gmail.com>
2022-11-30 19:05:44 +0530

    nav bar fixed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,94 +1,131 @@
-import { Input } from "@chakra-ui/input";
-import { ColorModeScript } from '@chakra-ui/react';
-import { useColorMode } from '@chakra-ui/react'
-import {
-  Heading,
-  Box,
-  Text,
-  Stack,
-  VStack,
-  useBreakpointValue,
-  Button,
-  ButtonGroup,
-  Container,
-  Flex,
-  HStack,
-  IconButton,
-  useColorModeValue,
-  color,
-} from "@chakra-ui/react";
-import React from "react";
+import { ReactNode } from "react";
 import logo from "../assets/logo.png";
-// import React from 'react';
-import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
-// import React from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Image,
+  useColorMode,
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  AddIcon,
+  SunIcon,
+  MoonIcon,
+} from "@chakra-ui/icons";
 
-function NavBar() {
-  const isDesktop = useBreakpointValue({ base: false, lg: true });
-  const { colorMode, toggleColorMode } = useColorMode()
+import { Link as RouterLink } from "react-router-dom";
+const Links = [
+  { name: "Home", link: "/" },
+  { name: "Register", link: "/register" },
+  { name: "Student Details", link: "/details" },
+];
+
+const NavLink = ({ children, to }: { children: ReactNode; to: string }) => (
+  <Link
+    as={RouterLink}
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    to={to}
+  >
+    {children}
+  </Link>
+);
+
+export default function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box as="section" pb={{ base: "12", md: "24" }}>
-      <Box
-        as="nav"
-        bg="#6d00af"
-
-        boxShadow={useColorModeValue("sm", "sm-dark")}
-        
-      >
-        <Container py={{ base: "4", lg: "5" }}>
-          <HStack spacing="3">
-            <img
-              src={logo}
-              height="50px"
-              width="50px"
-              style={{
-                position: "relative",
-                right: "100%"
-            }}/>
-            {
-            // isDesktop ? (
-              <Flex justify="space-between" flex="1">
-                <ButtonGroup
-                  variant="link"
-                  spacing="6"
-                  style={{
-                    position: "relative",
-                    right: "80%",
-                  }}
-                >
-                  <text color="Black">
-                    <b>No Proxy</b>
-                  </text>
-                </ButtonGroup>
-                <HStack spacing="8">
-                <Button variant="ghost">
-                    <Link to={"/home"}>Home</Link>
-                  </Button>
-                  <Button variant="ghost">
-                    <Link to={"/register"}>Register</Link>
-                  </Button>
-                  <Button variant="ghost">
-                    <Link to={"/details"}>Student Details</Link>
-                  </Button>
-                  <Button onClick={toggleColorMode}>
-                        {colorMode === 'light' ? 'Dark' : 'Light'}
-                  </Button>
-                </HStack>
-              </Flex>
-           // ) : (
-            //   // <IconButton
-            //   //   variant="ghost"
-            //   //   icon={<FiMenu fontSize="1.25rem" />}
-            //   //   aria-label="Open Menu"
-            //   // />
-            // )
+    <>
+      <Box backgroundColor="primary" px="4" py="2">
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"lg"}
+            icon={
+              isOpen ? (
+                <CloseIcon fontSize={"2xl"} />
+              ) : (
+                <HamburgerIcon fontSize={"2xl"} />
+              )
             }
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            variant="outline"
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Image src={logo} height="16" objectFit="cover" />
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link.name} to={link.link}>
+                  {link.name}
+                </NavLink>
+              ))}
+            </HStack>
           </HStack>
-        </Container>
+          <Flex alignItems={"center"}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+            {/* <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <Avatar
+                  size={"sm"}
+                  src={
+                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu> */}
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link.name} to={link.link}>
+                  {link.name}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
-    </Box>
+    </>
   );
 }
-
-export default NavBar;
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
 });
